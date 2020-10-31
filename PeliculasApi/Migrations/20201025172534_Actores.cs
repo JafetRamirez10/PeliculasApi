ca1b65e08319815cb5df7c847e30cefe9561e57f
Jafet Ramirez <jafet0810@gmail.com>
2020-10-31 14:44:07 -0600

    Agregar archivos de proyecto.

---
+++ PeliculasApi/Migrations/20201025172534_Actores.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PeliculasApi.Migrations
+{
+    public partial class Actores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(maxLength: 120, nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(nullable: false),
+                    Foto = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autores");
+        }
+    }
+}
